c6e5879cb7326c1ccf44578ce3b186d9e3427e34
Jbaldui <61895428+Jbaldui@users.noreply.github.com>
2022-05-29 20:03:48 -0300

    Correcao method put store

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,15 @@ import { Router } from "express";
 import {usersRoutes} from './users.routes'
 import {statementRouter} from './statement.routes'
 import {authRoutes} from './auth.routes'
+import {storesRoutes} from './stores.routes'
+
 const router = Router();
 
 // router.use("/categories", categoriesRoutes);
 router.use("/auth", authRoutes);
 router.use("/users", usersRoutes);
 router.use("/statement", statementRouter);
+router.use("/stores", storesRoutes);
 // router.use(autenticateRoutes);
 
 export { router };
