a1d7163fd95dfd4ae5f65a874a577538330dd4dd
Jbaldui <61895428+Jbaldui@users.noreply.github.com>
2022-05-29 17:42:38 -0300

    FavRoutes

--- src/modules/accounts/StoresRepository.js
+++ src/modules/accounts/StoresRepository.js
@@ -10,4 +10,48 @@ async function ListAllStores(res) {
     }
 }
 
-export { ListAllStores }
+async function FavoriteStore(req, res) {
+    const { idLoja } = req.body
+    const user = { email, dt_nascimento, nome, senha, cpf }
+    try {
+
+        let StoreExists = await findStoreByID(idLoja);
+
+        if (!StoreExists) {
+            throw new Error('Operação cancelada, loja inesistente!')
+        }
+
+        let IndexOfFavorited = await CheckFavorited(StoreExists, user.cpf)
+
+        if (IndexOfFavorited >= 0) {
+            //se achou o CPF do user, remove o CPF do array de quem favortou essa loja (unfavorita)
+            StoreExists.favoritedBy.splice(IndexOfFavorited, 1)
+            await StoreExists.save();
+            res.status(201).json({ message: "Essa loja deixou de ser sua favorita!" })
+        }
+        else
+            //se não achou o CPF, da um .push no cpf em StoreExists.favoritedBy
+            StoreExists.favoritedBy.push(user.cpf)
+            await StoreExists.save();
+            res.status(201).json({ message: "Essa loja se tornou sua favorita!" })
+
+    } catch (error) {
+        const errorMessage = error.toString()
+        res.status(400).json(errorMessage)
+    }
+
+}
+
+async function findStoreByID(idLoja) {
+    const store = await store.findOne({ idLoja: idLoja });
+
+    return store;
+}
+
+async function CheckFavorited(StoreExists, Cpf) {
+    const index = await StoreExists.favoritedBy.indexOf(Cpf);
+
+    return index;
+}
+
+export { ListAllStores, FavoriteStore }
